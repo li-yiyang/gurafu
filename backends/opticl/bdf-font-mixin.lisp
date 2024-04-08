6e07aa8c020a750be0c44200665a61ea2a0b183f
凉凉 <liyiyang@ryomac.local>
2024-04-08 20:25:26 +0800

    Remove `time' for bdf-font-mixin.
    Haha... I should split all the test codes and real codes...

--- backends/opticl/bdf-font-mixin.lisp
+++ backends/opticl/bdf-font-mixin.lisp
@@ -1,20 +1,19 @@
 (in-package :gurafu/backends/opticl)
 
-(time
- (let ((font-table (make-hash-table)))
-   ;; Load fonts from fonts directory
-   (loop for path in (uiop:directory-files
-                      (asdf:system-relative-pathname
-                       :gurafu
-                       "backends/opticl/fonts/"))
-         ;; Ensure load BDF Fonts only
-         if (string= (pathname-type path) "bdf")
-           do (let ((font (make-keyword (string-upcase (pathname-name path)))))
-                (setf (gethash font font-table)
-                      (load-bdf-font path))))
-   
-   (defparameter *opticl-bdf-fonts* font-table
-     "Opticl BDF fonts database. ")))
+(let ((font-table (make-hash-table)))
+  ;; Load fonts from fonts directory
+  (loop for path in (uiop:directory-files
+                     (asdf:system-relative-pathname
+                      :gurafu
+                      "backends/opticl/fonts/"))
+        ;; Ensure load BDF Fonts only
+        if (string= (pathname-type path) "bdf")
+          do (let ((font (make-keyword (string-upcase (pathname-name path)))))
+               (setf (gethash font font-table)
+                     (load-bdf-font path))))
+  
+  (defparameter *opticl-bdf-fonts* font-table
+    "Opticl BDF fonts database. "))
 
 (defclass bdf-font-mixin ()
   ()
