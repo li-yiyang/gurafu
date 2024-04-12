a853becbab6e86fd9f63ba617bab34176311d7ba
凉凉 <liyiyang@ryomac.local>
2024-04-12 20:56:19 +0800

    Updated the `linear-color-map' macro to force at least two colors.

--- core/colors.lisp
+++ core/colors.lisp
@@ -12,13 +12,14 @@
 
 ;; ========== linear-color-map ==========
 
-(defmacro linear-color-map (&rest colors)
-  "Make a linear color map function.
+(defmacro linear-color-map (color1 color2 &rest more-colors)
+  "Make a linear color map function. At least two colors should provided. 
 Return a lambda function to map unit weight (0 to 1) to colors. "
   (loop with weight = 0.0
-        with w = (float (/ 1.0 (1- (length colors))))
-        with c-vars = (loop for i below (length colors)
-                            collect (gensym "C"))
+        with colors = (cons color1 (cons color2 more-colors))
+        with size   = (length colors)
+        with w      = (float (/ 1.0 (1- size)))
+        with c-vars = (loop for i below size collect (gensym "C"))
         
         for (c1 c2) on c-vars
         while c2
@@ -41,13 +42,13 @@ Return a lambda function to map unit weight (0 to 1) to colors. "
 ;; 
 ;; Ref: https://zh.wikipedia.org/zh/中国传统色彩
 
-(defconstant +绛+
+(defparameter +绛+
   '(0.36862746 0.011764706 0.07058824)
   "比赤更深的红色.
 
 《说文》曰: “绛, 大赤也”. ")
 
-(defconstant +赤+
+(defparameter +赤+
   '(0.7647059 0.15294118 0.16862746)
   "介于绛色与朱色之间的红色.
 
@@ -56,17 +57,17 @@ Return a lambda function to map unit weight (0 to 1) to colors. "
 《说文》曰: “赤, 南方色也. ”
 可知赤亦是形容火与代表南方的颜色. ")
 
-(defconstant +朱+
+(defparameter +朱+
   '(1.0 0.0 0.0)
   "传统上的正红色, 最纯正的红色. ")
 
-(defconstant +丹+
+(defparameter +丹+
   '(1.0 0.29803923 0.0)
   "靓丽的朱色.
 
 《说文》曰: 丹, 巴越之赤石也. 赤石即朱砂, 故丹即朱砂色. ")
 
-(defconstant +红+
+(defparameter +红+
   '(1.0 0.7019608 0.654902)
   "浅红.
 
@@ -75,45 +76,45 @@ Return a lambda function to map unit weight (0 to 1) to colors. "
 又据清人段玉裁对《说文》中“红”一字的加注: “此今人所谓粉红、桃红也”.
 可知传统的红色, 即近世的粉红色. ")
 
-(defconstant +妃红+
+(defparameter +妃红+
   '(0.92941177 0.34117648 0.21176471)
   "浅红的一种, 用以描述上流女眷涂抹的脂粉与汗水混合而形成的颜色.
 
 一说是绯匹之色. ")
 
-(defconstant +品红+
+(defparameter +品红+
   '(0.9411765 0.0 0.3372549)
   "略浅于大红的颜色, 用以形容观赏性植物一品红的叶子的颜色. ")
 
-(defconstant +海棠+
+(defparameter +海棠+
   '(0.85882354 0.3529412 0.41960785)
   "传统上对海棠花颜色的形容.
 指苹果属梨花海棠的色泽, 呈较桃红色深一些的淡紫红色, 是非常妩媚娇艳的颜色. ")
 
-(defconstant +胭脂+
+(defparameter +胭脂+
   '(0.5882353 0.0 0.09411765)
   "女子装扮时所用胭脂的颜色, 亦作国画暗红色颜料的颜色. ")
 
-(defconstant +绯+
+(defparameter +绯+
   '(0.78431374 0.23529412 0.13725491)
   "深红的一种.
 
 徐铉校补《说文》曰: “绯, 帛赤色也”. ")
 
-(defconstant +赭+
+(defparameter +赭+
   '(0.6117647 0.3254902 0.2)
   "朱褐混合的颜色.
 
 《说文》曰: “赭, 赤土也”. ")
 
-(defconstant +茜+
+(defparameter +茜+
   '(0.69411767 0.20784314 0.27450982)
   "从茜草中提取的大红色. ")
 
-(defconstant +殷红+
+(defparameter +殷红+
   '(0.69411767 0.20784314 0.27450982)
   "类似荔枝皮的色泽, 用以描述发黑的红色. ")
 
-(defconstant +桃红+
+(defparameter +桃红+
   '(0.95686275 0.4745098 0.5137255)
   "桃花呈现的淡红, 比粉红略鲜润的颜色. ")
